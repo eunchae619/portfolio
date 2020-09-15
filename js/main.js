a45521acebea71b4601dd75c9880fa028f28a1e9
eunchae619 <eunchae619@naver.com>
2020-09-16 00:16:34 +0900

    프로젝트 부분 완료

--- js/main.js
+++ js/main.js
@@ -1,5 +1,39 @@
 navWrapper = document.querySelector(".nav_wrapper");
 categories = navWrapper.querySelectorAll("a");
+projects = document.querySelectorAll(".project_one_set");
+
+// project hover 이벤트
+for (const project of projects) {
+  project.addEventListener("mouseover", () => {
+    project.classList.add("move_bottom_20");
+  });
+
+  project.addEventListener("mouseout", () => {
+    project.classList.remove("move_bottom_20");
+  });
+
+  project.addEventListener("click", () => {
+    project.querySelector(".project_hidden_detail").classList.add("set_flex");
+
+    setTimeout(() => {
+      project
+        .querySelector(".project_hidden_detail")
+        .classList.add("set_opacity_1");
+    }, 100);
+  });
+
+  project.querySelector(".jsCloseBox").addEventListener("click", () => {
+    console.log("zz");
+    project
+      .querySelector(".project_hidden_detail")
+      .classList.add("set_opacity_0");
+    setTimeout(() => {
+      project
+        .querySelector(".project_hidden_detail")
+        .classList.remove("set_opacity_1", "set_opacity_0", "set_flex");
+    }, 500);
+  });
+}
 
 // intro 높이설정
 document
@@ -28,7 +62,7 @@ window.addEventListener("scroll", () => {
 });
 
 // Smooth Scroll 부분
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+document.querySelectorAll('.nav_wrapper a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
     navWrapper.classList.remove("set_navWrapper_spread");
